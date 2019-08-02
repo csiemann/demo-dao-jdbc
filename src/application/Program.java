595d71ab108844c1a36193f72b92616a846f448f
Caetano Siemann <caetanosiemann@gmail.com>
2019-08-02 11:50:17 -0300

    Seller.java created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dp = new Department(1, "books");
-		System.out.println(dp);
+		Seller s = new Seller(23, "Bob", "bob@gmail.com", new Date(), 1000.00, dp);
+		System.out.println(s);
 	}
 
 }
